8acbcac44e053c9b3d1463b3b128b915978340a5
Pedro Sánchez Casas <pedro.s.casas@tokiota.com>
2018-01-19 13:48:05 +0100

    remove hashtag value form url and create not found page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Page does not exists</h1>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import NotFound from '@/components/NotFound'
 import Todo from '@/components/Todo'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '*',
+      'component': NotFound
     },
     {
-      path: '/todos',
+      path: '/',
       name: 'Todo',
       component: Todo
     }
